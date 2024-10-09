ac35af8192507c4eea4db110c560d76fffd5e959
linventif <linventif@gmail.com>
2024-10-09 10:16:55 +0200

    exo 2

---
+++ run-cloud-init.sh
@@ -0,0 +1,5 @@
+rm seed.img disk.qcow2
+cp debian-12-genericcloud-amd64.qcow2 disk.qcow2
+cloud-localds seed.img user-data.yaml
+kvm -m 1024 -device virtio-net,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -hda disk.qcow2 -drive file=seed.img,format=raw
+echo "Run 'ssh -p 2222 debian@localhost' to connect to the VM"
